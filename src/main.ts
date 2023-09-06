2f8eb19e22ae4a1c9d6b5b0e78a53e4ddc5d70b2
Juan Echavarria <code.jfe@gmail.com>
2023-09-05 23:53:42 -0500

    Get user by id func added without repository structure yet

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import express from "express";
 
 import { config } from "./config";
 import { healthRouter } from "./health/health-router";
+import { userRouter } from "./users/user-router";
 
 function boostrap() {
   const app = express();
 
   app.use(express.json());
   app.use("/health", healthRouter);
+  app.use("/user", userRouter);
 
   const { port } = config.server;
 
